723c99d5f96bcae91185acb8dcb804d5a407f36a
RBLONESTAR <ronald.beck@lonestar.edu>
2026-09-04 12:32:59 -0500

    Create NumberProcessor class for number handling
    This class provides methods for reading, storing, and processing a set of integers from a file, including sorting and calculating product, minimum, and maximum values.

---
+++ NumberProcessor.java
@@ -0,0 +1,47 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class NumberProcessor {
+
+    // TODO: Declare the required instance variables.
+
+    public NumberProcessor() {
+        // TODO: Initialize the instance variables.
+    }
+
+    public void readNumbers(String filename) {
+        // TODO: Read and validate 20 integers from the file.
+    }
+
+    public int[] getNumbers() {
+        // TODO: Return the numbers.
+        return null;
+    }
+
+    public void setNumbers(int[] numbers) {
+        // TODO: Set the numbers.
+    }
+
+    public int[] getSortedNumbers() {
+        // TODO: Return the numbers in ascending order.
+        return null;
+    }
+
+    public BigInteger getProduct() {
+        // TODO: Calculate the product.
+        return BigInteger.ZERO;
+    }
+
+    public int getMinimum() {
+        // TODO: Find the minimum.
+        return 0;
+    }
+
+    public int getMaximum() {
+        // TODO: Find the maximum.
+        return 0;
+    }
+}
