62c305e2544b3cd867ed3c8df71a893b4c206751
RBLONESTAR <ronald.beck@lonestar.edu>
2026-09-04 12:33:43 -0500

    Implement initial ArrayQueue class structure
    Initial implementation of ArrayQueue with method stubs.

---
+++ ArrayQueue.java
@@ -0,0 +1,28 @@
+public class ArrayQueue {
+
+    // TODO: Declare the required instance variables.
+
+    public ArrayQueue() {
+        // TODO: Initialize the queue.
+    }
+
+    public void enqueue(int value) {
+        // TODO: Add a value to the back of the queue.
+    }
+
+    public int dequeue() {
+        // TODO: Remove and return the value at the front.
+        return 0;
+    }
+
+    public boolean isEmpty() {
+        // TODO: Return whether the queue is empty.
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        // TODO: Return the current queue contents.
+        return "";
+    }
+}
